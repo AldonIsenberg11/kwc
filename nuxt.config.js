d3dee55bbdd4fd73a0053f42526629a537f5e0fc
Ambra <ambra.isenberg@gmail.com>
2020-10-13 15:46:02 -0500

    importing template

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,24 +1,20 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'ksWrestlingCenter',
+    title: "ksWrestlingCenter",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -26,15 +22,15 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
     // Simple usage
-    '@nuxtjs/vuetify'
+    "@nuxtjs/vuetify"
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios"
     // https://go.nuxtjs.dev/content
     // '@nuxt/content'
   ],
@@ -47,6 +43,5 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   // buildDir: 'nuxt-dist',
-  build: {
-  }
-}
+  build: {}
+};
