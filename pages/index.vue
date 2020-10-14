8dc7b113dbcde81a9be75770646d9fd8a36d342b
Ambra <ambra.isenberg@gmail.com>
2020-10-14 10:16:52 -0500

    deleted extra script

--- pages/index.vue
+++ pages/index.vue
@@ -22,7 +22,6 @@
           href="https://cdn.vuetifyjs.com/images/logos/favicon.ico"
         />
         <div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v8.0" nonce="c3mTi1OH"></script>
       </head>
       <body>
         <div id="app">
@@ -141,7 +140,7 @@
                     class="mx-auto title font-weight-light mb-8"
                     max-width="1000"
                   >
-                    KC Wrestling K.W.C. provides premium facilities for hosting Wrestling & M.M.A driven events. We provide premium venue services with a focus on innovating the user experience.
+                    KC Wrestling K.W.C. provides premium facilities for hosting Wrestling and M.M.A driven events. We provide premium venue services with a focus on innovating the user experience.
                   <br><br>
                   **20 WRESTLING MATS** is the amount of space we are talking about. We aim to drive economic growth and and enhance the quality of wrestling life by providing facilities that are safe, versatile and active. Send us a message if you wish to know more.
                   </v-responsive>
@@ -260,7 +259,7 @@
                   </v-responsive>
 
                   <v-row>
-                                        <iframe
+                  <iframe
                     src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FksWrestlingCenter&tabs=timeline&width=500&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=true&appId"
                     width="500"
                     height="700"
@@ -303,7 +302,9 @@
                   <h2
                     class="display-2 font-weight-bold mb-3 text-uppercase text-center"
                   >
-                    Contact Me
+                    Questions?
+                    <br><br>
+                    Contact Us!
                   </h2>
 
                   <v-responsive class="mx-auto mb-12" width="56">
@@ -368,55 +369,55 @@ new Vue({
   vuetify: new Vuetify(),
   data() {
     return {
-      articles: [
-        {
-          src:
-            "https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-          title: "Mobile first & Responsive",
-          text:
-            "Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula eget tortor tempor, quis porta tellus dictum."
-        },
-        {
-          src:
-            "https://images.unsplash.com/photo-1475938476802-32a7e851dad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
-          title: "Think outside the box",
-          text:
-            "Nam ut leo ipsum. Maecenas pretium aliquam feugiat. Aenean vel tempor est, vitae tincidunt risus. Sed sodales vestibulum nibh."
-        },
-        {
-          src:
-            "https://images.unsplash.com/photo-1416339442236-8ceb164046f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1892&q=80",
-          title: "Small changes, big difference",
-          text:
-            "Vestibulum in dictum velit, in rhoncus nibh. Maecenas neque libero, interdum a dignissim in, aliquet vitae lectus. Phasellus lorem enim, luctus ut velit eget."
-        }
-      ],
-      features: [
-        {
-          icon: "mdi-account-group-outline",
-          title: "Vibrant Community",
-          text:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cupiditate sint possimus quidem atque harum excepturi nemo velit tempora! Enim inventore fuga, qui ipsum eveniet facilis obcaecati corrupti asperiores nam"
-        },
-        {
-          icon: "mdi-update",
-          title: "Frequent Updates",
-          text:
-            "Sed ut elementum justo. Suspendisse non justo enim. Vestibulum cursus mauris dui, a luctus ex blandit. Lorem ipsum dolor sit amet consectetur adipisicing elit. qui ipsum eveniet facilis obcaecati corrupti consectetur adipisicing elit."
-        },
-        {
-          icon: "mdi-shield-outline",
-          title: "Long-term Support",
-          text:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cupiditate sint possimus quidem atque harum excepturi nemo velit tempora! Enim inventore fuga, qui ipsum eveniet facilis obcaecati corrupti asperiores nam"
-        }
-      ],
-      stats: [
-        ["24k", "Github Stars"],
-        ["330+", "Releases"],
-        ["1m", "Downloads/mo"],
-        ["5m", "Total Downloads"]
-      ]
+      // articles: [
+      //   {
+      //     src:
+      //       "https://images.unsplash.com/photo-1423784346385-c1d4dac9893a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+      //     title: "Mobile first & Responsive",
+      //     text:
+      //       "Phasellus lorem enim, luctus ut velit eget, convallis egestas eros. Sed ornare ligula eget tortor tempor, quis porta tellus dictum."
+      //   },
+      //   {
+      //     src:
+      //       "https://images.unsplash.com/photo-1475938476802-32a7e851dad1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+      //     title: "Think outside the box",
+      //     text:
+      //       "Nam ut leo ipsum. Maecenas pretium aliquam feugiat. Aenean vel tempor est, vitae tincidunt risus. Sed sodales vestibulum nibh."
+      //   },
+      //   {
+      //     src:
+      //       "https://images.unsplash.com/photo-1416339442236-8ceb164046f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1892&q=80",
+      //     title: "Small changes, big difference",
+      //     text:
+      //       "Vestibulum in dictum velit, in rhoncus nibh. Maecenas neque libero, interdum a dignissim in, aliquet vitae lectus. Phasellus lorem enim, luctus ut velit eget."
+      //   }
+      // ],
+      // features: [
+      //   {
+      //     icon: "mdi-account-group-outline",
+      //     title: "Vibrant Community",
+      //     text:
+      //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cupiditate sint possimus quidem atque harum excepturi nemo velit tempora! Enim inventore fuga, qui ipsum eveniet facilis obcaecati corrupti asperiores nam"
+      //   },
+      //   {
+      //     icon: "mdi-update",
+      //     title: "Frequent Updates",
+      //     text:
+      //       "Sed ut elementum justo. Suspendisse non justo enim. Vestibulum cursus mauris dui, a luctus ex blandit. Lorem ipsum dolor sit amet consectetur adipisicing elit. qui ipsum eveniet facilis obcaecati corrupti consectetur adipisicing elit."
+      //   },
+      //   {
+      //     icon: "mdi-shield-outline",
+      //     title: "Long-term Support",
+      //     text:
+      //       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cupiditate sint possimus quidem atque harum excepturi nemo velit tempora! Enim inventore fuga, qui ipsum eveniet facilis obcaecati corrupti asperiores nam"
+      //   }
+      // ],
+      // stats: [
+      //   ["24k", "Github Stars"],
+      //   ["330+", "Releases"],
+      //   ["1m", "Downloads/mo"],
+      //   ["5m", "Total Downloads"]
+      // ]
     };
   }
 });
